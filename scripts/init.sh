ef863fdda4abe496798e5dc6a5b77bbd0098e6ce
zzztuiz <none>
2023-03-28 23:18:44 +0700

    update

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,18 +1,12 @@
-curl -Lo memfix.zip https://github.com/nolanaatama/sd-webui/raw/main/memfix.zip
-unzip /content/memfix.zip
-apt install -qq libunwind8-dev
-dpkg -i *.deb
-export LD_PRELOA=Dlibtcmalloc.so
-rm *
 pip install --upgrade fastapi==0.90.1
 git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui
-git clone https://github.com/nolanaatama/sd-webui-tunnels /content/stable-diffusion-webui/extensions/sd-webui-tunnels
+git clone https://github.com/Bing-su/sd-webui-tunnels /content/stable-diffusion-webui/extensions/sd-webui-tunnels
 git clone https://github.com/Mikubill/sd-webui-controlnet /content/stable-diffusion-webui/extensions/sd-webui-controlnet
 git clone https://github.com/fkunn1326/openpose-editor /content/stable-diffusion-webui/extensions/openpose-editor
 git clone https://github.com/yfszzx/stable-diffusion-webui-images-browser /content/stable-diffusion-webui/extensions/stable-diffusion-webui-images-browser
 git clone https://github.com/DominikDoom/a1111-sd-webui-tagcomplete /content/stable-diffusion-webui/extensions/a1111-sd-webui-tagcomplete
 # Model Code
-curl -Lo /content/stable-diffusion-webui/models/Stable-diffusion/realisticvisionv1.3.ckpt https://huggingface.co/nolanaatama/rv13/resolve/main/rv13.ckpt
+curl -Lo /content/stable-diffusion-webui/models/Stable-diffusion/realisticphoto.ckpt https://huggingface.co/dreamlike-art/dreamlike-photoreal-2.0/resolve/main/dreamlike-photoreal-2.0.safetensors
 # ControlNet
 curl -Lo /content/stable-diffusion-webui/extensions/sd-webui-controlnet/models/control_canny.safetensors https://huggingface.co/webui/ControlNet-modules-safetensors/resolve/main/control_canny-fp16.safetensors
 curl -Lo /content/stable-diffusion-webui/extensions/sd-webui-controlnet/models/control_depth.safetensors https://huggingface.co/webui/ControlNet-modules-safetensors/resolve/main/control_depth-fp16.safetensors
@@ -32,9 +26,4 @@ curl -Lo /content/stable-diffusion-webui/extensions/sd-webui-controlnet/models/t
 curl -Lo /content/stable-diffusion-webui/extensions/sd-webui-controlnet/models/t2iadapter_style_sd14v1.pth https://huggingface.co/TencentARC/T2I-Adapter/resolve/main/models/t2iadapter_style_sd14v1.pth
 rm -rf /content/stable-diffusion-webui/embeddings
 cd /content/stable-diffusion-webui
-git checkout 0cc0ee1
-git clone https://huggingface.co/nolanaatama/embeddings
-cd /content/stable-diffusion-webui/models
-git clone https://huggingface.co/nolanaatama/ESRGAN
-cd /content/stable-diffusion-webui
-COMMANDLINE_ARGS="--share --disable-safe-unpickle --no-half-vae --xformers --reinstall-xformers --enable-insecure-extension- --gradio-queue --remotemoe" REQS_FILE="requirements.txt" python launch.py
+COMMANDLINE_ARGS="--share --no-half-vae --xformers --reinstall-xformers --gradio-queue --remotemoe" REQS_FILE="requirements.txt" python launch.py
