09e838dd3c510ee684942c79537f8abf14b2c010
zzztuiz <none>
2023-03-31 20:13:47 +0700

    update

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,6 +1,7 @@
 mkdir /workspace
 cd /workspace
 
+apt-get update && apt-get install ffmpeg libsm6 libxext6  -y
 wget http://launchpadlibrarian.net/367274644/libgoogle-perftools-dev_2.5-2.2ubuntu3_amd64.deb
 wget https://launchpad.net/ubuntu/+source/google-perftools/2.5-2.2ubuntu3/+build/14795286/+files/google-perftools_2.5-2.2ubuntu3_all.deb
 wget https://launchpad.net/ubuntu/+source/google-perftools/2.5-2.2ubuntu3/+build/14795286/+files/libtcmalloc-minimal4_2.5-2.2ubuntu3_amd64.deb
